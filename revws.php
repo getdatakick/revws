b245167948f3ac84721b9cd2eb026fcdedc0a27b
Petr Hučík <petr.hucik@gmail.com>
2018-02-10 18:17:14 +0100

    bump version

--- revws.php
+++ revws.php
@@ -41,7 +41,7 @@ class Revws extends Module {
   public function __construct() {
     $this->name = 'revws';
     $this->tab = 'administration';
-    $this->version = '1.0.2';
+    $this->version = '1.0.3';
     $this->author = 'DataKick <petr@getdatakick.com>';
     $this->need_instance = 0;
     $this->bootstrap = true;
