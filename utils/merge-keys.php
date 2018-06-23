7794e2caeda0114cdf51f750657d051a9905aa92
Petr Hučík <petr.hucik@gmail.com>
2018-06-23 09:26:02 +0200

    eng strings

--- utils/merge-keys.php
+++ utils/merge-keys.php
@@ -8,27 +8,29 @@ $keys = getKeys($translations);
 foreach (glob("translations/*.json") as $file) {
   $lang = str_replace('.json', '', basename($file));
   $translated = mergeKeys($file, $keys);
-  $cnt = 0;
-  $lines = [];
-  foreach ($translations as $md5 => $t) {
-    $text = $t['text'];
-    if (isset($translated[$text])) {
-      $cnt++;
-      foreach ($t['files'] as $file) {
-        $trankey = "<{revws}prestashop>{$file}_{$md5}";
-        $lines[$trankey] = $translated[$text];
+  if ($lang != 'en') {
+    $cnt = 0;
+    $lines = [];
+    foreach ($translations as $md5 => $t) {
+      $text = $t['text'];
+      if (isset($translated[$text])) {
+        $cnt++;
+        foreach ($t['files'] as $file) {
+          $trankey = "<{revws}prestashop>{$file}_{$md5}";
+          $lines[$trankey] = $translated[$text];
+        }
       }
     }
-  }
-  $perc = round(10000 * $cnt / $total) / 100;
-  ksort($lines);
-  if ($lines) {
-    $content = "<?php\n\nglobal \$_MODULE;\n\$_MODULE = array();\n";
-    foreach ($lines as $key=>$value) {
-      $content .= "\$_MODULE['$key'] = '".str_replace("'", "\\'", $value)."';\n";
+    $perc = round(10000 * $cnt / $total) / 100;
+    ksort($lines);
+    if ($lines) {
+      $content = "<?php\n\nglobal \$_MODULE;\n\$_MODULE = array();\n";
+      foreach ($lines as $key=>$value) {
+        $content .= "\$_MODULE['$key'] = '".str_replace("'", "\\'", $value)."';\n";
+      }
+      $output = "./build/staging/revws/translations/$lang.php";
+      file_put_contents($output, $content);
+      echo "Translation '$lang': $cnt strings translated [ $perc% ]\n";
     }
-    $output = "./build/staging/revws/translations/$lang.php";
-    file_put_contents($output, $content);
-    echo "Translation '$lang': $cnt strings translated [ $perc% ]\n";
   }
 }
