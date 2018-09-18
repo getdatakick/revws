f80bcadafb335462058edd0448b54574a2406ace
Petr Hučík <petr@getdatakick.com>
2018-09-18 10:22:58 +0200

    change table key

--- php/sql/install.sql
+++ php/sql/install.sql
@@ -83,10 +83,12 @@ CREATE TABLE IF NOT EXISTS `PREFIX_revws_review_reaction` (
 ) ENGINE=ENGINE_TYPE DEFAULT CHARSET=CHARSET_TYPE;
 
 CREATE TABLE IF NOT EXISTS `PREFIX_revws_review_image` (
+  `id_image`      INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `id_review`     INT(11) UNSIGNED NOT NULL,
   `image`         VARCHAR(256) NOT NULL,
   `pos`           INT(11) NOT NULL,
-  PRIMARY KEY (`id_review`, `image`)
+  PRIMARY KEY (`id_image`),
+  KEY `id_review` (`id_review`)
 ) ENGINE=ENGINE_TYPE DEFAULT CHARSET=CHARSET_TYPE;
 
 INSERT IGNORE INTO `PREFIX_revws_criterion`(`id_criterion`, `global`) VALUES (1, 1);
--- php/sql/update-review-image.sql
+++ php/sql/update-review-image.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS `PREFIX_revws_review_image` (
+  `id_image`      INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `id_review`     INT(11) UNSIGNED NOT NULL,
   `image`         VARCHAR(256) NOT NULL,
   `pos`           INT(11) NOT NULL,
-  PRIMARY KEY (`id_review`, `image`)
+  PRIMARY KEY (`id_image`),
+  KEY `id_review` (`id_review`)
 ) ENGINE=ENGINE_TYPE DEFAULT CHARSET=CHARSET_TYPE;
