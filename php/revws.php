da25a280ca79c6fc5c5ab4071dfd944669313c57
Petr Hučík <petr@getdatakick.com>
2019-02-14 15:52:45 +0100

    bump version to 1.0.23

--- php/revws.php
+++ php/revws.php
@@ -60,7 +60,7 @@ class Revws extends Module {
   public function __construct() {
     $this->name = 'revws';
     $this->tab = 'administration';
-    $this->version = '1.0.22';
+    $this->version = '1.0.23';
     $this->author = 'DataKick';
     $this->need_instance = 0;
     $this->bootstrap = true;
